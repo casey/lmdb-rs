6628c5de45d2a3d1c9a69607ae217d9383b0954f
Casey Rodarmor <casey@rodarmor.com>
2022-03-12 13:59:11 -0800

    Use ord-liblmdb-sys fork

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ extern crate libc;
 #[macro_use] extern crate bitflags;
 #[macro_use] extern crate log;
 
-extern crate liblmdb_sys as ffi;
+extern crate ord_liblmdb_sys as ffi;
 
 pub use libc::c_int;
 pub use ffi::{mdb_filehandle_t, MDB_stat, MDB_envinfo, MDB_val};
